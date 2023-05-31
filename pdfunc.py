8cbfdde364f452e89a49bc8cbba97abffcb85094
Jordan Godwin <jordan.godwin13@gmail.com>
2023-05-31 10:08:00 -0500

    Fixed 'incorrect number of bindings supplied' error.
    Added ()s and comma to pkid to make it tuple, changing the amount of characters the program was reading from however many were input to a single value.

--- pdfunc.py
+++ pdfunc.py
@@ -5,8 +5,8 @@ def findByID():
 
     con = sqlite3.connect('pokedex.db')
     cur = con.cursor()
-    pkid = str(input('Which ID would you like to search for?\n'))
-    res = cur.execute("SELECT * FROM pokedex WHERE id = ?", pkid)
+    pkid = input('Which ID would you like to search for?\n')
+    res = cur.execute("SELECT * FROM pokedex WHERE id = ?", (pkid,))
     print(res.fetchall())
 
 
