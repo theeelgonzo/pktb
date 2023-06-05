f576ea3feec06e36ea08c85936166e38088096bf
Jordan Godwin <jordan.godwin13@gmail.com>
2023-06-05 13:53:32 -0500

    Add obs module. Define monster class.

---
+++ obs.py
@@ -0,0 +1,10 @@
+
+# define monster class
+
+class Monster:
+    def __init__(self, name, primType, subType, hp, level):
+        self.name = name
+        self.primType = primType
+        self.subType = None
+        self.hp = 10
+        self.level = 1
