1ee6e8e496cebcdb02dadbda7b31e8d309ce7202
Jordan Godwin <jordan.godwin13@gmail.com>
2023-06-06 05:58:04 -0500

    Add moveRoster and compatibility array attributes to monster object.

--- obs.py
+++ obs.py
@@ -2,12 +2,15 @@
 # define monster class
 
 class Monster:
-    def __init__(self, name, primType, subType, hp, level):
+    def __init__(self, name, primType, subType, hp, level, movesRoster, hmComp, tmComp):
         self.name = name
         self.primType = primType
         self.subType = None
         self.hp = 10
         self.level = 1
+        self.movesRoster = []
+        self.hmComp = []
+        self.tmComp = []
 
 # define trainer class
 
