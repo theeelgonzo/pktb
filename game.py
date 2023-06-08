e9f57c58920830a8a49f59bed97f0ec315b0f469
Jordan Godwin <jordan.godwin13@gmail.com>
2023-06-08 13:32:00 -0500

    Add startGame and newGame functions.

--- game.py
+++ game.py
@@ -1,5 +1,14 @@
 # start game
 # called from main
 
+import varStore as vs
+
 def startGame():
     print('Would you like to play a game?')
+    newGame = input('Would you like to\n1.Start A New Game?')
+    if newGame == '1':
+        newGame()
+    else:
+        vs.doof()
+
+def newGame():
